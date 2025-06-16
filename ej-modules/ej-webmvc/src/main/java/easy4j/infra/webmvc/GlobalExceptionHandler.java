33e7caba226af99c08d86dac2bba76379e0667d6
bokun <libokun@ewell.cc>
2025-06-16 15:23:24 +0800

    feat(web): webmvc add valid

--- ej-modules/ej-webmvc/src/main/java/easy4j/infra/webmvc/GlobalExceptionHandler.java
+++ ej-modules/ej-webmvc/src/main/java/easy4j/infra/webmvc/GlobalExceptionHandler.java
@@ -14,10 +14,15 @@
  */
 package easy4j.infra.webmvc;
 
+import cn.hutool.core.convert.Convert;
+import cn.hutool.core.util.ReflectUtil;
+import cn.hutool.core.util.StrUtil;
 import easy4j.infra.base.starter.env.Easy4j;
 import easy4j.infra.common.exception.EasyException;
+import easy4j.infra.common.exception.ExceptionList;
 import easy4j.infra.common.header.EasyResult;
 import easy4j.infra.common.i18n.I18nUtils;
+import easy4j.infra.common.utils.BusCode;
 import easy4j.infra.common.utils.SysLog;
 import easy4j.infra.context.Easy4jContext;
 import easy4j.infra.context.api.dblog.Easy4jDbLog;
@@ -25,8 +30,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -75,43 +82,52 @@ public class GlobalExceptionHandler {
     }
 
 
-
-    /*@ExceptionHandler(value = AspectValidateException.class)
-    @ResponseBody
-    public Map<String, Object> aspectValidateExceptionHandler(HttpServletRequest req, AspectValidateException e) throws Exception {
-        log.info("验证数据时产生异常-------"+e.getMessage());
-        return new RestResult().createResult("", e.getMessage(), e.getError());
-    }
+//    @ExceptionHandler(value = AspectValidateException.class)
+//    @ResponseBody
+//    public Map<String, Object> aspectValidateExceptionHandler(HttpServletRequest req, AspectValidateException e) throws Exception {
+//        log.info("验证数据时产生异常-------"+e.getMessage());
+//        return new RestResult().createResult("", e.getMessage(), e.getError());
+//    }
 
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
     @ResponseBody
-    public Map<String, Object> methodArgumentNotValidExceptionHandler(HttpServletRequest req, MethodArgumentNotValidException e) throws Exception {
-        log.info("Controller验证数据时产生异常-------"+e.getMessage());
+    public EasyResult<Object> methodArgumentNotValidExceptionHandler(HttpServletRequest req, MethodArgumentNotValidException e) throws Exception {
+        log.info("Controller验证数据时产生异常-------" + e.getMessage());
         ExceptionList<String> list = new ExceptionList<String>();
-        for(ObjectError o : e.getBindingResult().getAllErrors()){
-            list.add(o.getDefaultMessage());
+        for (ObjectError o : e.getBindingResult().getAllErrors()) {
+            String fieldValue = Convert.toStr(ReflectUtil.getFieldValue(o, "field"), "");
+            list.add((StrUtil.isNotBlank(fieldValue) ? "【" + fieldValue + "】" : "") + o.getDefaultMessage());
         }
-        return new RestResult().createResult(null, list.toString(), OpenApiErrorCode.PARAM_VALIDATE_ERROR);
-    }*/
+        try {
+            Easy4jDbLog easy4jDbLog = easy4jContext.get(Easy4jDbLog.class);
+            String message = e.getMessage();
+            RuntimeException wrapException = new RuntimeException(message);
+            easy4jDbLog.setException(wrapException);
+        } catch (Exception e2) {
+            Easy4j.info(SysLog.compact("context get has a error:" + e2.getMessage()));
+            e.printStackTrace();
+        }
+        return EasyResult.errorInfo(list.toString());
+    }
 
     @ExceptionHandler(value = HttpMessageNotReadableException.class)
     @ResponseBody
     public EasyResult<Object> httpErrorHandler(HttpServletRequest req, HttpMessageNotReadableException e) throws Exception {
         log.info("HTTP请求参数异常-------" + e.getMessage());
-        return EasyResult.errorInfo(I18nUtils.getMessage("A00005", e.getMessage()));
+        return EasyResult.errorInfo(I18nUtils.getMessage(BusCode.A00005, e.getMessage()));
     }
 
     @ExceptionHandler(value = HttpMediaTypeNotSupportedException.class)
     @ResponseBody
     public EasyResult<Object> httpMediaTypeNotSupportedExceptionHandler(HttpServletRequest req, HttpMediaTypeNotSupportedException e) throws Exception {
         log.info("HTTP请求ContentType异常-------" + e.getMessage());
-        return EasyResult.errorInfo(I18nUtils.getMessage("A00006"));
+        return EasyResult.errorInfo(I18nUtils.getMessage(BusCode.A00006));
     }
 
     @ExceptionHandler(value = HttpRequestMethodNotSupportedException.class)
     @ResponseBody
     public EasyResult<Object> httpMethodErrorHandler(HttpServletRequest req, HttpRequestMethodNotSupportedException e) throws Exception {
-        return EasyResult.errorInfo(I18nUtils.getMessage("A00007", e.getMessage()));
+        return EasyResult.errorInfo(I18nUtils.getMessage(BusCode.A00007, e.getMessage()));
     }
 }
 
