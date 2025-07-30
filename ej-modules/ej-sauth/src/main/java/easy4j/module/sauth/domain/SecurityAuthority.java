98dfb8f402b3edd231ebfaebf815a94ae58bab54
bokun <libokun@ewell.cc>
2025-07-30 16:59:36 +0800

    fix: fix can only get requests to cross domains

--- ej-modules/ej-sauth/src/main/java/easy4j/module/sauth/domain/SecurityAuthority.java
+++ ej-modules/ej-sauth/src/main/java/easy4j/module/sauth/domain/SecurityAuthority.java
@@ -14,6 +14,7 @@
  */
 package easy4j.module.sauth.domain;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -25,67 +26,79 @@ import java.util.Map;
  * 权限：字符串
  */
 @Data
+@Schema(description = "通用权限")
 public class SecurityAuthority implements Serializable {
 
     private static final long serialVersionUID = 1L;
     /**
      * 角色代码
      */
+    @Schema(description = "角色代码")
     private String roleCode;
 
     /**
      * 角色名称
      */
+    @Schema(description = "角色名称")
     private String roleName;
 
     /**
      * 权限代码
      */
+    @Schema(description = "权限代码")
     private String authorityCode;
 
 
     /**
      * 权限名称
      */
+    @Schema(description = "权限名称")
     private String authorityName;
 
 
     /**
      * 菜单code
      */
+    @Schema(description = "菜单code")
     private String menuCode;
 
 
     /**
      * 菜单名称
      */
+    @Schema(description = "菜单名称")
     private String menuName;
 
 
     /**
      * 请求地址 ant风格 /api/**
      */
+    @Schema(description = "请求地址 ant风格 /api/**")
     private String requestUri;
 
     /**
      * 是否有效
      */
+    @Schema(description = "是否有效")
     private boolean isEnabled;
 
     /**
      * 权限类别 菜单、应用、列表、按钮等
      */
+    @Schema(description = "权限类别 菜单、应用、列表、按钮等")
     private String authorityType;
 
 
     /**
      * 权限组
      */
+    @Schema(description = "权限组")
     private String group;
 
     /**
      * 额外信息
      */
+    @Schema(description = "额外信息")
     private Map<String, Object> extMap;
 
 }
--- ej-modules/ej-webmvc/src/main/java/easy4j/infra/webmvc/WebMvcCorsConfig.java
+++ ej-modules/ej-webmvc/src/main/java/easy4j/infra/webmvc/WebMvcCorsConfig.java
@@ -44,7 +44,7 @@ public class WebMvcCorsConfig implements WebMvcConfigurer, InitializingBean {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")  // 匹配所有接口
                 .allowedOriginPatterns("*")  // 允许所有来源（生产环境建议指定具体域名）
-                .allowedMethods("GET, POST, PUT, DELETE, OPTIONS")  // 允许所有请求方法
+                .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")  // 允许所有请求方法
                 .allowedHeaders("*")  // 允许所有请求头
                 .allowCredentials(false) // 允许携带 Cookie（需与前端配合，域名需一致）
                 .maxAge(3600); // 预检请求的有效期（秒）
