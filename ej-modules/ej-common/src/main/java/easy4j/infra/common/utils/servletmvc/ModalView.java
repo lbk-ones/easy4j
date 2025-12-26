888f17772f4d6ecd30e43300094127d8fab8a714
bokun <libokun@ewell.cc>
2025-12-26 19:26:25 +0800

    feat: perf ModalView
    (cherry picked from commit a126af4e66957899b9d4e7ef0c7a92be50125e49)

--- ej-modules/ej-common/src/main/java/easy4j/infra/common/utils/servletmvc/ModalView.java
+++ ej-modules/ej-common/src/main/java/easy4j/infra/common/utils/servletmvc/ModalView.java
@@ -1,9 +1,11 @@
 package easy4j.infra.common.utils.servletmvc;
 
+import cn.hutool.core.bean.BeanUtil;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
 import java.io.Serializable;
+import java.util.HashMap;
 import java.util.Map;
 
 @Data
@@ -12,7 +14,7 @@ public class ModalView implements Serializable {
 
     private String view;
 
-    private Map<String, Object> modal;
+    private Map<String, Object> modal = new HashMap<>();
 
     /**
      * 默认使用 THYMELEAF
@@ -20,4 +22,31 @@ public class ModalView implements Serializable {
     private ViewEngine viewEngine;
 
     private MimeType mimeType = MimeType.TEXT_HTML;
+
+    public static ModalView of(String viewPath) {
+        return new ModalView().setView(viewPath);
+    }
+
+    public static ModalView of(String viewPath, String key, Object value) {
+        return new ModalView().setView(viewPath).put(key, value);
+    }
+
+    public static ModalView of(String viewPath, Object object) {
+        return new ModalView().setView(viewPath).fromObj(object);
+    }
+
+    public static ModalView of(String viewPath, Map<String, Object> object) {
+        return new ModalView().setView(viewPath).setModal(object);
+    }
+
+    public ModalView put(String name, Object obj) {
+        modal.put(name, obj);
+        return this;
+    }
+
+    public ModalView fromObj(Object object) {
+        Map<String, Object> parseMap = BeanUtil.beanToMap(object);
+        if (parseMap != null && !parseMap.isEmpty()) modal.putAll(parseMap);
+        return this;
+    }
 }
