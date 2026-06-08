1f3c83de6b1de47d4dac536f2d7404b57977ec77
bokun <libokun@nhdc.cn>
2026-06-08 13:39:01 +0800

    fix: fix access the sentinel console

--- ej-modules/ej-sentinel/src/main/java/easy4j/module/sentinel/InitConfig.java
+++ ej-modules/ej-sentinel/src/main/java/easy4j/module/sentinel/InitConfig.java
@@ -17,16 +17,19 @@ public class InitConfig {
             if(init) return;
             init = true;
             // 初始化 Sentinel 核心
-            InitExecutor.doInit();
+
             // 配置 Dashboard 地址（如果配置了）
             Boolean enabled = Easy4j.getProperty(SysConstant.EASY4J_SENTINEL_DASHBOARD_ENABLE, Boolean.class, false);
             String url = Easy4j.getProperty(SysConstant.EASY4J_SENTINEL_DASHBOARD_CONSOLE_URL, String.class,"");
             Integer runtimePort = Easy4j.getProperty(SysConstant.EASY4J_SENTINEL_DASHBOARD_RUNTIME_PORT, Integer.class, 8179);
             if(enabled && runtimePort>0 && StrUtil.isNotBlank(url)){
                 TransportConfig.setRuntimePort(runtimePort);
-                SentinelConfig.setConfig(TransportConfig.CONSOLE_SERVER,url);
+                SentinelConfig.setConfig(TransportConfig.SERVER_PORT,String.valueOf(runtimePort));
                 SentinelConfig.setConfig(TransportConfig.CONSOLE_SERVER,url);
                 SentinelConfig.setConfig(SentinelConfig.PROJECT_NAME_PROP_KEY,Easy4j.getProperty(SysConstant.SPRING_SERVER_NAME));
+                SentinelConfig.setConfig(SentinelConfig.APP_NAME_PROP_KEY,Easy4j.getProperty(SysConstant.SPRING_SERVER_NAME));
+
+                InitExecutor.doInit();
             }
         }
 
